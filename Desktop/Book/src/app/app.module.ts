a99516bd0942e65d8505998e1a2033f0fa7baca0
HauNguyen <haunguyen0528@gmail.com>
2019-07-11 15:30:05 +0700

    first commit

---
+++ Desktop/Book/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserformComponent } from './userform/userform.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserformComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
