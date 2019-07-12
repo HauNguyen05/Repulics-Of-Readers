c181e7d39ff6b8412d2cae7aba60da56a85409a5
HauNguyen <haunguyen0528@gmail.com>
2019-07-12 15:51:56 +0700

    a

---
+++ ROR/Book/src/app/userform/Book.model.ts
@@ -0,0 +1,5 @@
+export class Book {
+    constructor(public title: string, public author: string, public type: string, public publisher: string, public date: string, public language: string, public country: string, public pages: number, public hashtag: string, public content: string) { }
+    
+
+}
